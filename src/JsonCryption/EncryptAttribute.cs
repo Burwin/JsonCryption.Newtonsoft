4507546c503feb1ce73612cbc665a451dacdd8b5
burwin <19492392+Burwin@users.noreply.github.com>
2020-01-20 19:16:13 -0500

    better to spell my own project correctly

---
+++ src/JsonCryption/EncryptAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace JsonCryption
+{
+    public sealed class EncryptAttribute : JsonConverterAttribute
+    {
+        public EncryptAttribute() : base(typeof(EncryptedJsonConverterFactory))
+        {
+        }
+    }
+}
---
+++ src/JsonCryption/EncryptedJsonConverterFactory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace JsonCryption
+{
+    internal class EncryptedJsonConverterFactory : JsonConverterFactory
+    {
+        public override bool CanConvert(Type typeToConvert) => throw new NotImplementedException();
+        public override JsonConverter CreateConverter(Type typeToConvert, JsonSerializerOptions options) => throw new NotImplementedException();
+    }
+}
