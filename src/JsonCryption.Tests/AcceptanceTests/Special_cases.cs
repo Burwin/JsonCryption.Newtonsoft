ceddb615a74ef26d0bb56326376d6783cc955dcf
burwin <19492392+Burwin@users.noreply.github.com>
2020-01-22 18:40:24 -0500

    some special cases, but System.Test.Json is crap so far

--- src/JsonCryption.Tests/AcceptanceTests/Special_cases.cs
+++ src/JsonCryption.Tests/AcceptanceTests/Special_cases.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Shouldly;
+using System;
+using System.Text.Json;
 using Xunit;
 
 namespace JsonCryption.Tests.AcceptanceTests
@@ -8,29 +10,118 @@ namespace JsonCryption.Tests.AcceptanceTests
         [Fact]
         public void One_property_but_not_the_other()
         {
+            Coordinator.ConfigureDefault(Helpers.GenerateRandomKey());
+
+            var foo = new FooMixedEncryption { EncryptedInt = 57, UnencryptedString = "foo"};
+            var json = JsonSerializer.Serialize(foo);
+
+            // make sure it's encrypted
+            using (var jsonDoc = JsonDocument.Parse(json))
+            {
+                var encryptedInt = jsonDoc.RootElement.GetProperty(nameof(FooMixedEncryption.EncryptedInt));
+                encryptedInt.ValueKind.ShouldBe(JsonValueKind.String);
+                encryptedInt.GetString().ShouldNotBe(JsonSerializer.Serialize(foo.EncryptedInt));
+
+                var unencryptedString = jsonDoc.RootElement.GetProperty(nameof(FooMixedEncryption.UnencryptedString));
+                unencryptedString.ValueKind.ShouldBe(JsonValueKind.String);
+                unencryptedString.GetString().ShouldBe(foo.UnencryptedString);
+            }
+
+            // decrypt and check
+            var decrypted = JsonSerializer.Deserialize<FooMixedEncryption>(json);
+            decrypted.UnencryptedString.ShouldBe(foo.UnencryptedString);
+            decrypted.EncryptedInt.ShouldBe(foo.EncryptedInt);
+        }
+
+        private class FooMixedEncryption
+        {
+            [Encrypt]
+            public int EncryptedInt { get; set; }
+            public string UnencryptedString { get; set; }
+        }
+
+        // field support not yet available for System.Text.Json
+        //[Fact]
+        //public void Fields_instead_of_properties()
+        //{
+        //    Coordinator.ConfigureDefault(Helpers.GenerateRandomKey());
+
+        //    var foo = new FooPublicFields();
+        //    foo.EncryptedInt = 57;
+        //    foo.UnencryptedString = "foo";
+
+        //    var json = JsonSerializer.Serialize(foo);
+
+        //    // make sure it's encrypted
+        //    using (var jsonDoc = JsonDocument.Parse(json))
+        //    {
+        //        var encryptedInt = jsonDoc.RootElement.GetProperty(nameof(FooPublicFields.EncryptedInt));
+        //        encryptedInt.ValueKind.ShouldBe(JsonValueKind.String);
+        //        encryptedInt.GetString().ShouldNotBe(JsonSerializer.Serialize(foo.EncryptedInt));
+
+        //        var unencryptedString = jsonDoc.RootElement.GetProperty(nameof(FooPublicFields.UnencryptedString));
+        //        unencryptedString.ValueKind.ShouldBe(JsonValueKind.String);
+        //        unencryptedString.GetString().ShouldBe(foo.UnencryptedString);
+        //    }
+
+        //    // decrypt and check
+        //    var decrypted = JsonSerializer.Deserialize<FooPublicFields>(json);
+        //    decrypted.UnencryptedString.ShouldBe(foo.UnencryptedString);
+        //    decrypted.EncryptedInt.ShouldBe(foo.EncryptedInt);
+        //}
+
+        //private class FooPublicFields
+        //{
+        //    [Encrypt]
+        //    public int EncryptedInt;
+        //    public string UnencryptedString;
+        //}
+
+        [Fact]
+        public void Internal_properties()
+        {
+            Coordinator.ConfigureDefault(Helpers.GenerateRandomKey());
+
+            var foo = new FooInternalProperties { MyInt = 57, MyString = "foo" };
+            var json = JsonSerializer.Serialize(foo);
+
+            // make sure it's encrypted
+            using (var jsonDoc = JsonDocument.Parse(json))
+            {
+                var encryptedInt = jsonDoc.RootElement.GetProperty(nameof(FooInternalProperties.MyInt));
+                encryptedInt.ValueKind.ShouldBe(JsonValueKind.String);
+                encryptedInt.GetString().ShouldNotBe(JsonSerializer.Serialize(foo.MyInt));
+
+                var unencryptedString = jsonDoc.RootElement.GetProperty(nameof(FooInternalProperties.MyString));
+                unencryptedString.ValueKind.ShouldBe(JsonValueKind.String);
+                unencryptedString.GetString().ShouldBe(foo.MyString);
+            }
+
+            // decrypt and check
+            var decrypted = JsonSerializer.Deserialize<FooInternalProperties>(json);
+            decrypted.MyString.ShouldBe(foo.MyString);
+            decrypted.MyInt.ShouldBe(foo.MyInt);
+
             throw new NotImplementedException();
         }
 
+        private class FooInternalProperties
+        {
+            [Encrypt]
+            internal int MyInt { get; set; }
+            
+            [Encrypt]
+            internal string MyString { get; set; }
+        }
+
         [Fact]
-        public void Fields_instead_of_properties()
+        public void Protected_properties()
         {
             throw new NotImplementedException();
         }
 
         [Fact]
-        public void Internal_properties_and_fields()
-        {
-            throw new NotImplementedException();
-        }
-
-        [Fact]
-        public void Protected_properties_and_fields()
-        {
-            throw new NotImplementedException();
-        }
-
-        [Fact]
-        public void Private_properties_and_fields()
+        public void Private_properties()
         {
             throw new NotImplementedException();
         }
--- src/JsonCryption/EncryptAttribute.cs
+++ src/JsonCryption/EncryptAttribute.cs
@@ -1,7 +1,9 @@
-﻿using System.Text.Json.Serialization;
+﻿using System;
+using System.Text.Json.Serialization;
 
 namespace JsonCryption
 {
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property | AttributeTargets.Class | AttributeTargets.Struct)]
     public sealed class EncryptAttribute : JsonConverterAttribute
     {
         public EncryptAttribute() : base(typeof(EncryptedJsonConverterFactory))
