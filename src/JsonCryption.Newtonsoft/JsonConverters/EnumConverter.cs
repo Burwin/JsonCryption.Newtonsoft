4b8fb692f0aa32cab941d97ad93609f837bad586
burwin <19492392+Burwin@users.noreply.github.com>
2020-01-29 19:24:19 -0500

    cleaning whitespace

--- src/JsonCryption.Newtonsoft/JsonConverters/EnumConverter.cs
+++ src/JsonCryption.Newtonsoft/JsonConverters/EnumConverter.cs
@@ -6,6 +6,7 @@ namespace JsonCryption.Newtonsoft.JsonConverters
     internal sealed class EnumConverter<T> : EncryptedConverter<T>
     {
         private static readonly string _purpose = typeof(EnumConverter<T>).FullName;
+
         public EnumConverter(IDataProtectionProvider dataProtectionProvider, IByteConverter<T> byteConverter)
             : base(dataProtectionProvider.CreateProtector(_purpose), byteConverter)
         {
