fa34d0bc8acc2465b040d50dab3976ce2ed99557
burwin <19492392+Burwin@users.noreply.github.com>
2020-03-03 22:59:03 -0500

    fix sonar code smell

--- src/JsonCryption.Utf8Json/ConstructorResolutionException.cs
+++ src/JsonCryption.Utf8Json/ConstructorResolutionException.cs
@@ -2,7 +2,7 @@
 
 namespace JsonCryption.Utf8Json
 {
-    internal sealed class ConstructorResolutionException : Exception
+    public sealed class ConstructorResolutionException : Exception
     {
         public ConstructorResolutionException(Type type)
             : base($"No suitable constructor found for type {type.Name}")
